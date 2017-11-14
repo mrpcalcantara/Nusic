9e5d9046e14ed49faf55f9c38f843fd651a99b34
Kageeee <mrpcalcantara@gmail.com>
2017-11-14 14:50:39 +0700

    Adding Interactive Gestures to Views

---
+++ Newsic/Interaction Controllers/CustomInteractionController.swift
@@ -0,0 +1,53 @@
+//
+//  CustomInteractionController.swift
+//  Newsic
+//
+//  Created by Miguel Alcantara on 14/11/2017.
+//  Copyright © 2017 Miguel Alcantara. All rights reserved.
+//
+
+import UIKit
+
+class CustomInteractionController: UIPercentDrivenInteractiveTransition {
+    
+    var navigationController: UINavigationController!
+    var shouldCompleteTransition = false
+    var transitionInProgress = false
+    var completionSeed: CGFloat {
+        return 1 - percentComplete
+    }
+    
+    func attachToViewController(viewController: UIViewController) {
+        navigationController = viewController.navigationController
+        setupGestureRecognizer(view: viewController.view)
+    }
+    
+    private func setupGestureRecognizer(view: UIView) {
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:))))
+    }
+    
+    @objc func handlePanGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
+        let viewTranslation = gestureRecognizer.translation(in: gestureRecognizer.view!.superview!)
+        switch gestureRecognizer.state {
+        case .began:
+            transitionInProgress = true
+            navigationController.popViewController(animated: true)
+            print("began")
+        case .changed:
+            var const = CGFloat(fminf(fmaxf(Float(viewTranslation.x / 2000.0), 0.0), 1.0))
+            shouldCompleteTransition = const > 0.9
+            update(const)
+            print("changed")
+        case .cancelled, .ended:
+            transitionInProgress = false
+            print("ended")
+            if !shouldCompleteTransition || gestureRecognizer.state == .cancelled {
+                cancel()
+            } else {
+                finish()
+            }
+        default:
+            print("Swift switch must be exhaustive, thus the default")
+        }
+    }
+}
--- Newsic/Interaction Controllers/CustomNavigationAnimationController.swift
+++ Newsic/Interaction Controllers/CustomNavigationAnimationController.swift
@@ -11,9 +11,15 @@ import UIKit
 class CustomNavigationAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
     
     var reverse: Bool = false
+    var slideDirection: UISwipeGestureRecognizerDirection;
+    
+    init(direction: UISwipeGestureRecognizerDirection) {
+        slideDirection = direction
+        super.init()
+    }
     
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 1.5
+        return 0.5
     }
 //
 //    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
@@ -68,7 +74,12 @@ class CustomNavigationAnimationController: NSObject, UIViewControllerAnimatedTra
         let containerView = transitionContext.containerView
         let bounds = UIScreen.main.bounds
         let rectOffset = CGRect(origin: finalFrameForVC.origin, size: finalFrameForVC.size)
-        toViewController.view.frame = rectOffset.offsetBy(dx: 0, dy: bounds.size.height)
+        var offsetDirection = self.slideDirection == .right ? rectOffset.offsetBy(dx: bounds.size.width, dy: 0) : rectOffset.offsetBy(dx: -bounds.size.width, dy: 0)
+        
+        if reverse {
+            offsetDirection.origin.x = offsetDirection.origin.x * -1
+        }
+        toViewController.view.frame = offsetDirection
         containerView.addSubview(toViewController.view)
         
         UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .curveLinear, animations: {
