309247c2c598732d959138e1d2e8b4dfa05979cd
Kageeee <mrpcalcantara@gmail.com>
2017-12-19 18:37:59 +0700

    Continuing Choice List menu

--- Newsic/View Controllers/Song Picker View Controller/Extensions/ChoiceListViewDelegate.swift
+++ Newsic/View Controllers/Song Picker View Controller/Extensions/ChoiceListViewDelegate.swift
@@ -8,38 +8,103 @@
 
 import UIKit
 
-extension SongPickerViewController : ChoiceListDelegate {
-    func didFinishPanHeader() {
+extension SongPickerViewController {
+    
+    func setupListMenu() {
+        listMenuView = ChoiceListView(frame: CGRect(x: self.view.frame.origin.x, y: self.view.frame.height, width: self.view.frame.width, height: self.view.frame.height - self.view.safeAreaInsets.top - self.navbar.frame.height))
         
+        self.listMenuView.delegate = self
+        self.view.addSubview(listMenuView)
     }
     
-    func didTapGenre() {
-        
+}
+
+extension SongPickerViewController : ChoiceListDelegate {
+    func isEmpty() {
+        closeChoiceMenu()
     }
-    func didTapMood() {
-        
+    
+    func isNotEmpty() {
+        showChoiceMenu()
     }
-    func didTapHeader(willOpen: Bool) {
-        if willOpen {
-            listMenuView.frame.origin.y = self.view.frame.height/2
-            listViewBottomConstraint.constant = self.view.frame.height/2
+    
+    func willMove(to point: CGPoint, animated: Bool) {
+        let minPointY = self.view.safeAreaInsets.top + self.navbar.frame.height + self.newsicControl.frame.origin.y + self.newsicControl.frame.height
+        let maxPointY = self.view.frame.height
+//        self.listViewBottomConstraint.constant = self.view.frame.height - point.y
+        if animated {
+            animateMove(to: point)
         } else {
-            listMenuView.frame.origin.y = self.view.frame.height - listMenuView.toggleViewHeight
-            listViewBottomConstraint.constant = listMenuView.toggleViewHeight
+            listMenuView.frame.origin.y = point.y
+            self.view.layoutIfNeeded()
         }
-        self.view.layoutIfNeeded()
+    }
+    
+    func didTapGenre(value: String) {
+//        if let firstCharacter = value.first?.description {
+//            manageSectionTitle(for: firstCharacter);
+//        }
+        if let indexPath = getIndexPathForGenre(value) {
+            if let genre = SpotifyGenres(rawValue: value) {
+                
+                sectionGenres[indexPath.section-1].append(genre)
+                sectionGenres[indexPath.section-1].sort(by: { (genre1, genre2) -> Bool in
+                    return genre1.rawValue < genre2.rawValue
+                })
+                selectedGenres.removeValue(forKey: genre.rawValue.lowercased());
+                var indexSet = IndexSet()
+                indexSet.insert(indexPath.section)
+                genreCollectionView.reloadSections(indexSet)
+//                genreCollectionView.reloadData()
+            }
+            
+        }
+    }
+    
+    func didTapMood(value: String) {
+//        if let indexPath = getIndexPathForGenre(value) {
+//            sectionGenres[indexPath.section].insert(SpotifyGenres(rawValue: value), at: indexPath.row)
+//        }
+    }
+    
+    func didTapHeader(willOpen: Bool) {
+        toggleChoiceMenu(willOpen: willOpen)
+        self.view.sizeToFit()
     }
     
     func didPanHeader(_ translationX: CGFloat, _ translationY: CGFloat) {
-        
-        print(translationY)
-        let minPointY = self.view.safeAreaInsets.top + self.navbar.frame.height + self.newsicControl.frame.origin.y + self.newsicControl.frame.height
-        let maxPointY = self.view.frame.height
-        if translationY > minPointY && translationY < maxPointY {
-            listViewBottomConstraint.constant = self.view.frame.height-translationY
-            listMenuView.frame.origin.y = translationY
-            self.view.layoutIfNeeded()
+        listViewBottomConstraint.constant = self.view.frame.height-translationY
+        self.view.layoutIfNeeded()
+        self.view.sizeToFit()
+    }
+    
+    func showChoiceMenu() {
+        self.listViewBottomConstraint.constant = self.view.frame.height*0.25 + 16
+        willMove(to: CGPoint(x: listMenuView.frame.origin.x, y: self.view.frame.height*0.75), animated: true)
+    }
+    
+    func hideChoiceMenu() {
+        self.listViewBottomConstraint.constant = listMenuView.toggleViewHeight + 16
+        willMove(to: CGPoint(x: listMenuView.frame.origin.x, y: self.view.frame.height - listMenuView.toggleViewHeight), animated: true)
+    }
+    
+    func closeChoiceMenu() {
+        self.listViewBottomConstraint.constant = 0
+        willMove(to: CGPoint(x: listMenuView.frame.origin.x, y: self.view.frame.height), animated: true)
+    }
+    
+    func toggleChoiceMenu(willOpen: Bool) {
+        if willOpen {
+            showChoiceMenu()
+        } else {
+            hideChoiceMenu()
         }
-        
+    }
+    
+    func animateMove(to point:CGPoint) {
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.listMenuView.frame.origin.y = point.y
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
 }
--- Newsic/View Controllers/Song Picker View Controller/Views/Mood Genre List/ChoiceList.swift
+++ Newsic/View Controllers/Song Picker View Controller/Views/Mood Genre List/ChoiceList.swift
@@ -9,11 +9,14 @@
 import UIKit
 
 @objc protocol ChoiceListDelegate: class {
-    func didTapGenre()
-    func didTapMood()
-    @objc optional func didTapHeader(willOpen: Bool)
-    @objc optional func didPanHeader(_ translationX: CGFloat, _ translationY: CGFloat)
-    @objc optional func didFinishPanHeader()
+    func didTapGenre(value: String)
+    func didTapMood(value: String)
+    func didTapHeader(willOpen: Bool)
+    func didPanHeader(_ translationX: CGFloat, _ translationY: CGFloat)
+    func willMove(to point:CGPoint, animated: Bool)
+    func isEmpty()
+    func isNotEmpty()
+//    @objc optional func didFinishPanHeader()
 }
 
 class ChoiceListView: NewsicView {
@@ -33,13 +36,30 @@ class ChoiceListView: NewsicView {
     //Bottom part: Collection View
     let choiceCollectionView: UICollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
     
+    var startY: CGFloat = 0
+    var maxY: CGFloat = 0
+    var closeThreshold: CGFloat = 0.25
+    var panProgress: CGFloat = 0
+    
     
     //Data
     var chosenMoods: [String] = []
     var hasMoods: Bool = false
-    var chosenGenres: [String] = []
+    var chosenGenres: [String] = [] {
+        didSet {
+            if chosenGenres.count == 0 {
+                delegate?.isEmpty()
+                isOpen = false
+            }
+        }
+    }
     var hasGenres: Bool = false
     var isOpen: Bool = false
+    
+    enum Section: Int {
+        case moodSection
+        case genreSection
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -51,9 +71,14 @@ class ChoiceListView: NewsicView {
     }
     
     func setupView() {
+        
         setupToggleView();
         setupCollectionView();
         setupGestureRecognizers()
+        
+        maxY = (UIApplication.shared.keyWindow?.frame.height)! - self.toggleView.frame.height
+        startY = self.frame.origin.y
+        
     }
     
     func setupToggleView() {
@@ -85,11 +110,13 @@ class ChoiceListView: NewsicView {
         choiceCollectionView.isScrollEnabled = true
         choiceCollectionView.bounces = true
         choiceCollectionView.alwaysBounceVertical = true
+        choiceCollectionView.autoresizingMask = [.flexibleHeight, .flexibleBottomMargin]
         
         choiceCollectionView.backgroundColor = UIColor.clear
         
         if let layout = choiceCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
             layout.scrollDirection = .vertical
+            layout.estimatedItemSize = UICollectionViewFlowLayoutAutomaticSize
         }
         
         
@@ -121,7 +148,7 @@ class ChoiceListView: NewsicView {
     @objc func toggleListMenu() {
         isOpen = !isOpen
         lastPanPoint = self.frame.origin
-        delegate?.didTapHeader!(willOpen: isOpen)
+        delegate?.didTapHeader(willOpen: isOpen)
     }
     
     @objc func handleListMenuPan(_ gestureRecognizer: UIPanGestureRecognizer) {
@@ -129,28 +156,78 @@ class ChoiceListView: NewsicView {
         switch gestureRecognizer.state {
         case .began:
             lastPanPoint = self.frame.origin
+            panProgress = 0
         case .changed:
-            delegate?.didPanHeader!(translation.x, lastPanPoint.y+translation.y);
+            let pointY = lastPanPoint.y+translation.y
+            panProgress = pointY / maxY
+            print("progress = \(panProgress)")
+            self.frame.origin.y = pointY
+            animateMove(to: CGPoint(x: self.frame.origin.x, y: pointY))
+            delegate?.didPanHeader(translation.x, pointY);
         case .cancelled:
             break;
         case .ended:
-            break;
+            isOpen = true
+            if panProgress > 1-closeThreshold || self.frame.origin.y > maxY {
+                animateMove(to: CGPoint(x: self.frame.origin.x, y: maxY))
+                delegate?.didPanHeader(translation.x, maxY)
+                isOpen = false
+            } else if panProgress < 0.25 {
+                animateMove(to: CGPoint(x: self.frame.origin.x, y: self.frame.height/4))
+                delegate?.didPanHeader(translation.x, self.frame.height/4)
+            }
         default:
             break
         }
+    }
+    
+    func animateMove(to point:CGPoint) {
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.frame.origin.y = point.y
+        }, completion: { (isCompleted) in
+            self.frame.size = CGSize(width: self.frame.width, height: (UIApplication.shared.keyWindow?.frame.height)! - self.frame.origin.y)
+        })
+        
+    }
+    
+    func insertChosenGenre(value: String) {
+        chosenGenres.append(value)
+        if !isOpen {
+            delegate?.isNotEmpty()
+            isOpen = true
+        }
+        choiceCollectionView.performBatchUpdates({
+            var indexSet = IndexSet(); indexSet.insert(1)
+            choiceCollectionView.reloadSections(indexSet)
+        }, completion: nil)
+        chosenGenres.sort()
     }
 }
 
 extension ChoiceListView: UICollectionViewDelegate {
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! MoodViewCell
+        if indexPath.section == Section.genreSection.rawValue {
+            chosenGenres.remove(at: indexPath.row)
+            delegate?.didTapGenre(value: (cell.moodLabel?.text)!)
+        } else {
+            chosenMoods.remove(at: indexPath.row)
+            delegate?.didTapMood(value: (cell.moodLabel?.text)!)
+        }
+        
+        collectionView.performBatchUpdates({
+            collectionView.deleteItems(at: [indexPath])
+        }, completion: nil)
+        
+        
+    }
+    
 }
 
 extension ChoiceListView: UICollectionViewDataSource {
     
-    enum Section: Int {
-        case moodSection
-        case genreSection
-    }
+    
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return Section.genreSection.rawValue+1
@@ -161,7 +238,7 @@ extension ChoiceListView: UICollectionViewDataSource {
         case Section.genreSection.rawValue:
             return chosenGenres.count
         case Section.moodSection.rawValue:
-            return chosenMoods.count 
+            return chosenMoods.count
         default:
             return 2
         }
@@ -195,11 +272,36 @@ extension ChoiceListView: UICollectionViewDataSource {
             cell.moodLabel.text = chosenMoods[indexPath.row]
         }
         
-        
-        
         return cell
     }
     
+}
+
+extension ChoiceListView: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var width: CGFloat = 100
+        let font = UIFont(name: "Futura", size: 18)
+        switch indexPath.section {
+        case Section.genreSection.rawValue:
+            width = chosenGenres[indexPath.row].width(withConstraintedHeight: 50, font: font!)
+        case Section.moodSection.rawValue:
+            width = chosenMoods[indexPath.row].width(withConstraintedHeight: 50, font: font!)
+        default:
+            break;
+        }
+        
+        return CGSize(width: width, height: 50)
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8;
+    }
     
 }
 
