3d23326624e0bd17a96fe8a794e97e6f02ba36e5
Miguel Alcântara <miguelalcantara@miguels-macbook-pro.local>
2018-02-15 17:30:02 +0700

    Reset badge counter to 0 on open

--- Newsic/Application Delegate/AppDelegate.swift
+++ Newsic/Application Delegate/AppDelegate.swift
@@ -117,11 +117,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        UIApplication.shared.applicationIconBadgeNumber = 0
         print("APP WILL ENTER FOREGROUND")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
         print("APP ACTIVE")
     }
 
