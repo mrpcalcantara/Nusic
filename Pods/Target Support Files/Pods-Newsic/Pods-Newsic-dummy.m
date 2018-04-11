1c978af4884b058e6bd5aea9d18d1542b2596e01
Kageeee <mrpcalcantara@gmail.com>
2018-04-11 14:01:26 +0700

    Adding pods

---
+++ Pods/Target Support Files/Pods-Newsic/Pods-Newsic-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Newsic : NSObject
+@end
+@implementation PodsDummy_Pods_Newsic
+@end
---
+++ Pods/Target Support Files/Pods-NewsicTests/Pods-NewsicTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewsicTests : NSObject
+@end
+@implementation PodsDummy_Pods_NewsicTests
+@end
